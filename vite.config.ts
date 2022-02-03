ffc8c1df5773b5a044558562e1abe95d90c5083c
Viacheslav Miroshko <ccctux@gmail.com>
2022-02-03 11:31:09 +0200

    added a few more plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import reactJsx from 'vite-react-jsx';
+import compress from 'vite-plugin-compress';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => {
@@ -9,7 +12,7 @@ export default defineConfig((configEnv) => {
     ? '[local]-[hash:base64:5]'
     : '[hash:base64:5]';
   return {
-    plugins: [react(), VitePWA()],
+    plugins: [react(), reactJsx(), VitePWA(), compress(), viteCompression()],
     css: {
       modules: {
         generateScopedName,
