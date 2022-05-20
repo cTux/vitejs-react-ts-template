a0b7e970430362168d7c756f206a4058205d19cd
Viacheslav Miroshko <viacheslav.miroshko@auto1.com>
2022-05-20 11:51:52 +0300

    chore(versions): updated vite* dependencies to latest versions

--- tests/vitest.enzyme.setup.ts
+++ tests/vitest.enzyme.setup.ts
@@ -1,7 +1,9 @@
+import fetch from 'node-fetch';
 import './mocks/i18next.vitest.mock';
-// eslint-disable-next-line
-// @ts-ignore
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { configure } from 'enzyme';
 
+// eslint-disable-next-line
+// @ts-ignore
+global.fetch = fetch;
 configure({ adapter: new Adapter() });
--- tests/vitest.testing-library.setup.ts
+++ tests/vitest.testing-library.setup.ts
@@ -1,1 +1,6 @@
+import fetch from 'node-fetch';
 import './mocks/i18next.vitest.mock';
+
+// eslint-disable-next-line
+// @ts-ignore
+global.fetch = fetch;
