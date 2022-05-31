7c15cc4641ebce86d3751d69e6fa89f421b3427c
Viacheslav Miroshko <ccctux@gmail.com>
2022-05-31 18:08:34 +0300

    fix(deps): removed i18n, added storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,6 @@ module.exports = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
--- src/components/ButtonsLanguage/utils/changeLanguage.ts
+++
@@ -1,3 +0,0 @@
-export const i18n = {
-  changeLanguage: (i18n, language: string) => i18n.changeLanguage?.(language),
-};
