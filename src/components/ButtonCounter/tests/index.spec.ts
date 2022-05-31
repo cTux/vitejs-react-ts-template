422bdbaa9a84d56e32208ef590b7d5e77c6a0771
Viacheslav Miroshko <ccctux@gmail.com>
2022-05-31 18:11:57 +0300

    fix(deps): removed i18n, added storybook build

--- src/components/ButtonCounter/tests/index.spec.ts
+++ src/components/ButtonCounter/tests/index.spec.ts
@@ -9,8 +9,8 @@ describe('ButtonCounter', () => {
 
   it('should increment counter', async () => {
     await driver.render();
-    expect(driver.component.get.text()).toEqual('btn.counter 0');
+    expect(driver.component.get.text()).toEqual('Counter: 0');
     driver.component.when.clicked();
-    expect(driver.component.get.text()).toEqual('btn.counter 1');
+    expect(driver.component.get.text()).toEqual('Counter: 1');
   });
 });
