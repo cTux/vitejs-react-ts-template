151ad46f22acbd6d87a1eb8dcf0b75dc0452c12e
Viacheslav Miroshko <viacheslav.miroshko@uptech.team>
2022-12-19 10:46:40 +0200

    fix(playwright): changed timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,18 +2,27 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const url = 'http://localhost:6006';
 const baseURL = `${url}/`;
+const timeout = 30000;
+const globalTimeout = 120000;
 
 const config: PlaywrightTestConfig = {
+  timeout,
+  globalTimeout,
+  expect: {
+    timeout,
+  },
   webServer: {
-    command: 'pnpm run dev:storybook',
-    timeout: 30000, // 30 seconds
+    timeout,
     url,
+    command: 'pnpm run dev:storybook',
     reuseExistingServer: !process.env.CI,
     ignoreHTTPSErrors: true,
   },
   use: {
-    headless: true,
     baseURL,
+    actionTimeout: timeout,
+    navigationTimeout: timeout,
+    headless: true,
     ignoreHTTPSErrors: true,
   },
 };
