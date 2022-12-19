6d586d4afae86e738959254634f4f6978954f445
Viacheslav Miroshko <viacheslav.miroshko@uptech.team>
2022-12-19 15:16:10 +0200

    chore(app): removed pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export const port = 5173;
@@ -13,7 +12,7 @@ export default defineConfig(
       port,
       strictPort: true,
     },
-    plugins: [react(), VitePWA(), basicSsl()],
+    plugins: [react(), basicSsl()],
     css: {
       modules: {
         generateScopedName:
