0f3be8beed511d0078698512ef348e6b0d22fe0b
Viacheslav Miroshko <ccctux@gmail.com>
2023-11-22 15:57:06 +0200

    feat(deps): vite5

--- scripts/add-template-remote.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git config remote.template.url >&- || git remote add template git@github.com:cTux/vitejs-react-ts-template.git
--- scripts/merge-template-changes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git fetch --all
-git merge template/master -m "chore(template): updated template" --allow-unrelated-histories
